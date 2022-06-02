dd17e0251ac4caedc8f1cbcc090b6ff835dea0ad
Pablo Perez <pablo.perez.rodriguez@udc.es>
2022-06-01 20:19:35 -0500

    Basic interactive shell for the project.

--- main.cpp
+++ main.cpp
@@ -3,32 +3,56 @@
 #include "eventGenerator/EventGeneratorFactory.h"
 #include "chronolog/ChronoLog.h"
 
-int main(int argc, char **argv) {
-    if (argc < 2) {
-        std::cout << "Usage: poc.exe <path_to_config_file>" << std::endl;
-        return -1;
+int mainLoop() {
+    std::string command;
+
+    std::cout << "ChronoSQL version 0.0.1" << std::endl << "Type \"help\" for help." << std::endl;
+
+    while (true) {
+        std::cout << "csql>";
+        std::getline(std::cin, command);
+
+        if (command == "help") {
+            std::cout << "Available commands:" << std::endl << std::endl
+                      << "exit: close the ChronoSQL interactive shell"
+                      << std::endl << "help: print help" << std::endl << "q: close the ChronoSQL interactive shell"
+                      << std::endl << std::endl << "More coming soon!" << std::endl;
+        } else if (command == "exit" || command == "q") {
+            break;
+        }
     }
 
-    auto *config = (new ConfigurationManager(argv[1]))->getConfiguration();
-
-    auto *generator = new KeyValueEventGenerator(config->payloadSize, config->payloadVariation);
-
-    auto *log = new ChronoLog(config);
-    log->record(0, generator->generateRandomBytes(config->payloadSize));
-    log->record(0, generator->generateRandomBytes(config->payloadSize));
-    log->record(0, generator->generateRandomBytes(config->payloadSize));
-    log->record(0, generator->generateRandomBytes(config->payloadSize));
-    log->record(0, generator->generateRandomBytes(config->payloadSize));
-    std::cout << log->playback() << std::endl;
-
-    // time_t timenum = (time_t) strtol(timestr, NULL, 10);
-
-    std::list<char *> events = log->replay(-1, -1);
-
-    int i = 0;
-    for (auto &event: events) {
-        std::cout << i++ << ": " << event << std::endl;
-    }
-
+    std::cout << std::endl << "Exiting...\n" << std::endl;
     return 0;
+}
+
+int main(int argc, char **argv) {
+//    if (argc < 2) {
+//        std::cout << "Usage: poc.exe <path_to_config_file>" << std::endl;
+//        return -1;
+//    }
+//
+//    auto *config = (new ConfigurationManager(argv[1]))->getConfiguration();
+//
+//    auto *generator = new KeyValueEventGenerator(config->payloadSize, config->payloadVariation);
+//
+//    auto *log = new ChronoLog(config);
+//    log->record(0, generator->generateRandomBytes(config->payloadSize));
+//    log->record(0, generator->generateRandomBytes(config->payloadSize));
+//    log->record(0, generator->generateRandomBytes(config->payloadSize));
+//    log->record(0, generator->generateRandomBytes(config->payloadSize));
+//    log->record(0, generator->generateRandomBytes(config->payloadSize));
+//    std::cout << log->playback() << std::endl;
+//
+//    // time_t timenum = (time_t) strtol(timestr, NULL, 10);
+//
+//    std::list<char *> events = log->replay(-1, -1);
+//
+//    int i = 0;
+//    for (auto &event: events) {
+//        std::cout << i++ << ": " << event << std::endl;
+//    }
+//
+//    return 0;
+    return mainLoop();
 }
